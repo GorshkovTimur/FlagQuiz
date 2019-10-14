cd2de985a26ae91bab063c0f6a7464e3ee37afb9
GorshkovTimur <gorshkoff.timur@gmail.com>
2019-10-14 23:47:46 +0300

    onStart on Create

--- app/src/main/java/com/timmyg/flagquiz/MainActivity.java
+++ app/src/main/java/com/timmyg/flagquiz/MainActivity.java
@@ -1,5 +1,7 @@
 package com.timmyg.flagquiz;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,9 +10,16 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.preference.PreferenceManager;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String CHOICES = "pref_numberOfChoices";
+    public static final String REGIONS = "pref_regionsToInclude";
+
+    private boolean phoneDevice = true;
+    private boolean preferenceChanged = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +28,29 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(preferenceChangeListener);
+
+        int screenSize = getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        if (screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE || screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE)
+            phoneDevice = false;
+
+        if (phoneDevice)
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(preferenceChanged){
+            MainActivityFragment quizFragment = (MainActivityFragment) getSupportFragmentManager().findFragmentById(R.id.quiz_fragment);
+            quizFragment.updateGuessRows(PreferenceManager.getDefaultSharedPreferences(this));
+            quizFragment.updateRegions (PreferenceManager.getDefaultSharedPreferences(this));
+            quizFragment.resetQuiz();
+            preferenceChanged = false;
+        }
+    }
 }
